44657e3e79f74187d39c79eddd64128f9c0c1041
Mark Ogden <mark.pm.ogden@btinternet.com>
2023-05-30 16:29:22 +0100

    dev-2023.5.30.5: cleaned build

--- l83/l83.c
+++ l83/l83.c
@@ -9,9 +9,6 @@
  ****************************************************************************/
 
 
-#define _CRT_SECURE_NO_WARNINGS
-#define _CRT_NONSTDC_NO_WARNINGS
-
 #include <stdio.h>
 #include <string.h>
 #include <memory.h>
